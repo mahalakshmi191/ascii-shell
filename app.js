55c7658a1e3a9524639ca067bf8f8d4bd19b90de
mahalakshmi191 <107101381+mahalakshmi191@users.noreply.github.com>
2026-03-31 17:10:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello from Docker!");
+  res.send('App is running 🚀');
 });
 
 const PORT = 3000;
 
-// 🚨 THIS LINE IS CRITICAL
+// 🔥 CRITICAL for Docker
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
